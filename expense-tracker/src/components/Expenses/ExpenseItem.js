e7f46d90f39090fd6ce676c043df8973a0325391
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-12 17:47:27 +0300

    //

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,15 @@
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import React from "react";
 
 const ExpenseItem = props => {
+  // const [title, setTitle] = React.useState(props.expense.title);
+
+  // const changeTitle = () => {
+  //   setTitle(prompt("enter new title"));
+  // };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
@@ -10,6 +17,7 @@ const ExpenseItem = props => {
         <h2>{props.expense.title}</h2>
         <div className="expense-item__price">&#8362;{props.expense.amount}</div>
       </div>
+      {/* <button onClick={changeTitle}>Change Title</button> */}
     </Card>
   );
 };
