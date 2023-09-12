4b3c5acdd355fc930c77ffbdb17f0cbe134b543a
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-12 15:01:30 +0300

    expense tracker

--- Chapter03/expense-tracker/src/components/ExpenseItem.js
+++ Chapter03/expense-tracker/src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = props => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
         <h2>{props.expense.title}</h2>
         <div className="expense-item__price">&#8362;{props.expense.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
---
+++ Chapter03/expense-tracker/src/components/Expenses.js
@@ -0,0 +1,15 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = props => {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((expense, i) => (
+        <ExpenseItem expense={expense} key={i} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
