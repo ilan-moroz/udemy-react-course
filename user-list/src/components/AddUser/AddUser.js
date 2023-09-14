087c8bc2ad09cbe5e8e5993995459d16d666605d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-14 18:24:39 +0300

    error modal

--- user-list/src/components/AddUser/AddUser.js
+++ user-list/src/components/AddUser/AddUser.js
@@ -1,4 +1,5 @@
 import Button from "../Button/Button";
+import ErrorModal from "../ErrorModal/ErrorModal";
 import classes from "./AddUser.module.css";
 import React from "react";
 
@@ -9,6 +10,7 @@ const INITIAL_STATE = {
 
 const AddUser = props => {
   const [formData, setFormData] = React.useState(INITIAL_STATE);
+  const [isError, SetIsError] = React.useState(false);
 
   const inputChangedHandler = (input, value) => {
     setFormData(prev => {
@@ -18,6 +20,10 @@ const AddUser = props => {
 
   const handleFormSubmit = e => {
     e.preventDefault();
+    if (formData.userName === "" && formData.age === "") {
+      SetIsError(true);
+      return;
+    }
     props.setUsers(prev => [...prev, formData]);
     formResetHandler();
   };
@@ -26,39 +32,50 @@ const AddUser = props => {
     setFormData(INITIAL_STATE);
   };
 
+  const removeErrorHandler = () => {
+    SetIsError(false);
+  };
+
   return (
-    <form className={classes.form} onSubmit={handleFormSubmit}>
-      <div className={classes["input-group"]}>
-        <p>
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={formData.userName}
-            onChange={e => inputChangedHandler("userName", e.target.value)}
-            required
-          />
-        </p>
-        <p>
-          <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            min="1"
-            step="1"
-            value={formData.age}
-            onChange={e => inputChangedHandler("age", e.target.value)}
-            required
-          />
-        </p>
-      </div>
-      <div className={classes.actions}>
-        <Button type={"submit"}>Add User</Button>
-        <Button type={"button"} onClick={formResetHandler}>
-          Reset
-        </Button>
-      </div>
-    </form>
+    <>
+      {isError && (
+        <ErrorModal
+          title="Invalid input"
+          msg="Please enter a valid name and age (non-empty values)!"
+          onClick={removeErrorHandler}
+        />
+      )}
+      <form className={classes.form} onSubmit={handleFormSubmit}>
+        <div className={classes["input-group"]}>
+          <p>
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              value={formData.userName}
+              onChange={e => inputChangedHandler("userName", e.target.value)}
+            />
+          </p>
+          <p>
+            <label htmlFor="age">Age (Years)</label>
+            <input
+              id="age"
+              type="number"
+              min="1"
+              step="1"
+              value={formData.age}
+              onChange={e => inputChangedHandler("age", e.target.value)}
+            />
+          </p>
+        </div>
+        <div className={classes.actions}>
+          <Button type={"submit"}>Add User</Button>
+          <Button type={"button"} onClick={formResetHandler}>
+            Reset
+          </Button>
+        </div>
+      </form>
+    </>
   );
 };
 
