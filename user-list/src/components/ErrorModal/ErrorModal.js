ae05e697e2a890f2e30a647358a27cae3a08019f
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-14 18:49:11 +0300

    -

--- user-list/src/components/ErrorModal/ErrorModal.js
+++ user-list/src/components/ErrorModal/ErrorModal.js
@@ -4,23 +4,21 @@ import classes from "./ErrorModal.module.css";
 
 const ErrorModal = props => {
   return (
-    <>
-      <div className={classes.backdrop} onClick={props.onClick}>
-        <Card className={classes.modal}>
-          <header className={classes.header}>
-            <h2>{props.title}</h2>
-          </header>
-          <div className={classes.content}>
-            <p>{props.msg}</p>
-          </div>
-          <footer className={classes.actions}>
-            <Button type={"button"} onClick={props.onClick}>
-              OK
-            </Button>
-          </footer>
-        </Card>
-      </div>
-    </>
+    <div className={classes.backdrop} onClick={props.onClick}>
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.msg}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button type={"button"} onClick={props.onClick}>
+            OK
+          </Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
