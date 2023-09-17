a92bb1cb0123eb90724efa6546ef1665e0b5a8a1
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-17 15:15:38 +0300

    header

---
+++ food-order/src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import mealImg from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>MorozMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImg} alt="table with food" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
