115d8c6eed62e52f7095b8cfd08c6d6b23293ece
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-22 19:11:35 +0300

    useEffect fetch

--- redux-advanced/src/App.js
+++ redux-advanced/src/App.js
@@ -2,9 +2,18 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import React from "react";
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  React.useEffect(() => {
+    fetch("https://react-http-fc3c5-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
