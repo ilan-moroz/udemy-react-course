da6695ccfc06c91e85972b22f1f7a058bf9abe19
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-22 18:02:31 +0300

    cartSlice

--- redux-advanced/src/store/cartSlice.js
+++ redux-advanced/src/store/cartSlice.js
@@ -1,14 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-  name: "ui",
-  initialState: { cartIsVisible: false },
+  name: "cart",
+  initialState: { cartItems: [], totalItems: 0 },
   reducers: {
-    toggleCart(state) {
-      state.cartIsVisible = !state.cartIsVisible;
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.cartItems.find(item => item.id === newItem.id);
+      if (!existingItem) {
+        state.cartItems.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.name,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.cartItems.find(item => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item => item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
     },
   },
 });
 
-export const uiActions = cartSlice.actions;
+export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
