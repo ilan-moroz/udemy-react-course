ff24301c0e91e1dd8d3610b172b393cd173a3075
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-23 14:09:31 +0300

    RootLayout.module.css

--- routing-basic/src/pages/RootLayout.js
+++ routing-basic/src/pages/RootLayout.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import MainNav from "../components/MainNav";
+import classes from "./RootLayout.module.css";
 
 const RootLayout = () => {
   return (
     <>
       <MainNav />
-      <Outlet />
+      <main className={classes.content}>
+        <Outlet />
+      </main>
     </>
   );
 };
