b0d82f7b0c810c7922696fbbaa20005b1bc96464
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-23 13:59:01 +0300

    Link

--- routing-basic/src/pages/Home.js
+++ routing-basic/src/pages/Home.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link to={"/products"}>Products Page</Link>
+    </>
+  );
 };
 
 export default Home;
