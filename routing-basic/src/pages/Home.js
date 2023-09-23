aa6d86d09dbd164644eac754c306e924c4742598
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-23 14:28:32 +0300

    useNavigate

--- routing-basic/src/pages/Home.js
+++ routing-basic/src/pages/Home.js
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>Home Page</h1>
       <Link to={"/products"}>Products Page</Link>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 };
