a1b65742e4eb1262827dd6e5b46a4df2b104b9b9
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-24 15:10:55 +0300

    loader

--- routing-advanced/frontend/src/routes/MainRoute.js
+++ routing-advanced/frontend/src/routes/MainRoute.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "../pages/RootLayout";
 import HomePage from "../pages/HomePage";
-import EventsPage from "../pages/EventsPage";
+import EventsPage, { loader } from "../pages/EventsPage";
 import EventDetailPage from "../pages/EventDetailPage";
 import NewEventPage from "../pages/NewEventPage";
 import EditEventPage from "../pages/EditEventPage";
@@ -23,15 +23,7 @@ const MainRoute = () => {
             {
               index: true,
               element: <EventsPage />,
-              loader: async () => {
-                const response = await fetch("http://localhost:8080/events");
-
-                if (!response.ok) {
-                } else {
-                  const resData = await response.json();
-                  return resData.events;
-                }
-              },
+              loader: loader,
             },
             { path: ":eventId", element: <EventDetailPage /> },
             { path: "new", element: <NewEventPage /> },
