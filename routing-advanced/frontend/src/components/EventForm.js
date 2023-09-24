0d50c7a5a671a97b91c4052168ef40b47b2782ea
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-24 18:25:52 +0300

    useActionData

--- routing-advanced/frontend/src/components/EventForm.js
+++ routing-advanced/frontend/src/components/EventForm.js
@@ -1,8 +1,14 @@
-import { useNavigate, Form, useNavigation } from "react-router-dom";
+import {
+  useNavigate,
+  Form,
+  useNavigation,
+  useActionData,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
@@ -14,13 +20,19 @@ function EventForm({ method, event }) {
 
   return (
     <Form method="post" className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map(err => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
           id="title"
           type="text"
           name="title"
-          required
           defaultValue={event ? event.title : ""}
         />
       </p>
@@ -30,7 +42,6 @@ function EventForm({ method, event }) {
           id="image"
           type="url"
           name="image"
-          required
           defaultValue={event ? event.image : ""}
         />
       </p>
@@ -40,7 +51,6 @@ function EventForm({ method, event }) {
           id="date"
           type="date"
           name="date"
-          required
           defaultValue={event ? event.date : ""}
         />
       </p>
@@ -50,7 +60,6 @@ function EventForm({ method, event }) {
           id="description"
           name="description"
           rows="5"
-          required
           defaultValue={event ? event.description : ""}
         />
       </p>
--- routing-advanced/frontend/src/pages/NewEventPage.js
+++ routing-advanced/frontend/src/pages/NewEventPage.js
@@ -24,6 +24,8 @@ export const action = async ({ request, params }) => {
     body: JSON.stringify(eventData),
   });
 
+  if (response.status === 422) return response;
+
   if (!response.ok) {
     throw json(
       { message: "Could not save the event" },
