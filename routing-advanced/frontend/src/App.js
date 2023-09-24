b99b9300a3fab8095f6a8fde922a6b17944df87c
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-24 13:52:44 +0300

    -

--- routing-advanced/frontend/src/App.js
+++ routing-advanced/frontend/src/App.js
@@ -1,5 +1,3 @@
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import MainRoute from "./routes/MainRoute";
 
 function App() {
