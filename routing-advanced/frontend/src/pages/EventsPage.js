82232f03ba34d789275149135c68de09fc4a6491
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-24 14:08:01 +0300

    events page

--- routing-advanced/frontend/src/pages/EventsPage.js
+++ routing-advanced/frontend/src/pages/EventsPage.js
@@ -1,48 +1,37 @@
-import classes from "./EventsPage.module.css";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "System Of A Down",
-    date: "28/5/2024",
-    price: 299,
-  },
-  {
-    id: "e2",
-    title: "Denzel Curry",
-    date: "28/10/2024",
-    price: 199,
-  },
-  {
-    id: "e3",
-    title: "Swissa",
-    date: "22/12/2023",
-    price: 149,
-  },
-  {
-    id: "e4",
-    title: "Vibe Ish",
-    date: "23/9/2023",
-    price: 100,
-  },
-];
+import EventsList from "../components/EventsList";
 
-const EventsPage = () => {
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <ul className={classes.events}>
-        {DUMMY_EVENTS.map(event => (
-          <li key={event.id} className={classes.event}>
-            <h2>{event.title}</h2>
-            <h5>{event.date}</h5>
-            <p>${event.price}</p>
-            <Link to={event.id}>Event detail</Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
-};
+}
 
 export default EventsPage;
