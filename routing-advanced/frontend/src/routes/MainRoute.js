e88ad0cca0db50680e0378f6c31ac6bf33d9e847
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-24 13:56:13 +0300

    /

--- routing-advanced/frontend/src/routes/MainRoute.js
+++ routing-advanced/frontend/src/routes/MainRoute.js
@@ -15,10 +15,10 @@ const MainRoute = () => {
       errorElement: <Page404 />,
       children: [
         { index: true, element: <HomePage /> },
-        { path: "/events", element: <EventsPage /> },
-        { path: "/events/:eventId", element: <EventDetailPage /> },
-        { path: "/events/new", element: <NewEventPage /> },
-        { path: "/events/:eventId/edit", element: <EditEventPage /> },
+        { path: "events", element: <EventsPage /> },
+        { path: "events/:eventId", element: <EventDetailPage /> },
+        { path: "events/new", element: <NewEventPage /> },
+        { path: "events/:eventId/edit", element: <EditEventPage /> },
       ],
     },
   ]);
