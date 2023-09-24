1a54dec6f017a3f782042083e1ac2ab6c9caa8ed
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-24 16:31:22 +0300

    action

--- routing-advanced/frontend/src/pages/NewEventPage.js
+++ routing-advanced/frontend/src/pages/NewEventPage.js
@@ -1,7 +1,35 @@
 import EventForm from "../components/EventForm";
+import { json } from "react-router-dom";
 
 const NewEventPage = () => {
   return <EventForm />;
 };
 
 export default NewEventPage;
+
+export const action = async ({ request, params }) => {
+  const data = await request.formData();
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not save the event" },
+      {
+        status: 500,
+      }
+    );
+  }
+};
--- routing-advanced/frontend/src/routes/MainRoute.js
+++ routing-advanced/frontend/src/routes/MainRoute.js
@@ -5,7 +5,7 @@ import EventsPage, { loader as eventLoader } from "../pages/EventsPage";
 import EventDetailPage, {
   loader as EventDetailLoader,
 } from "../pages/EventDetailPage";
-import NewEventPage from "../pages/NewEventPage";
+import NewEventPage, { action as newEventAction } from "../pages/NewEventPage";
 import EditEventPage from "../pages/EditEventPage";
 import Page404 from "../pages/Page404";
 import EventsRoot from "../pages/EventsRoot";
@@ -39,7 +39,7 @@ const MainRoute = () => {
                 { path: "edit", element: <EditEventPage /> },
               ],
             },
-            { path: "new", element: <NewEventPage /> },
+            { path: "new", element: <NewEventPage />, action: newEventAction },
           ],
         },
       ],
