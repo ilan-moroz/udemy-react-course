c5f306e214a62016765867b630c75db285e65033
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-27 15:43:16 +0300

    Await

--- routing-advanced/frontend/src/pages/EventsPage.js
+++ routing-advanced/frontend/src/pages/EventsPage.js
@@ -1,13 +1,16 @@
+import { Suspense } from "react";
 import EventsList from "../components/EventsList";
-import { useLoaderData, json, defer } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 function EventsPage() {
-  const data = useLoaderData();
-  if (data.isError) return <p>{data.message}</p>;
-
-  const events = data.events;
-
-  return <EventsList events={events} />;
+  const { events } = useLoaderData();
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Await resolve={events}>
+        {eventsData => <EventsList events={eventsData} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
@@ -16,16 +19,10 @@ const loadEvents = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: "Error loading events" };
-    // throw new Error({ message: "Error loading events" });
-    // throw new Response(JSON.stringify({ message: "Error loading events" }), {
-    //   status: 500,
-    // });
     throw json({ message: "Error loading events" }, { status: 500 });
   } else {
-    // const resData = await response.json();
-    // return resData.events;
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
 };
 
