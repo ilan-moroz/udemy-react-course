954385881220cf6200566e72fc84f7a5b9eaecf5
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-27 15:45:49 +0300

    defer await

--- routing-advanced/frontend/src/pages/EventsPage.js
+++ routing-advanced/frontend/src/pages/EventsPage.js
@@ -3,6 +3,11 @@ import EventsList from "../components/EventsList";
 import { useLoaderData, json, defer, Await } from "react-router-dom";
 
 function EventsPage() {
+  // const data = useLoaderData();
+  // if (data.isError) return <p>{data.message}</p>;
+  // const events = data.events;
+  // return <EventsList events={events} />;
+
   const { events } = useLoaderData();
   return (
     <Suspense fallback={<p>Loading...</p>}>
@@ -19,15 +24,22 @@ const loadEvents = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
+    // return { isError: true, message: "Error loading events" };
+    // throw new Error({ message: "Error loading events" });
+    // throw new Response(JSON.stringify({ message: "Error loading events" }), {
+    //   status: 500,
+    // });
     throw json({ message: "Error loading events" }, { status: 500 });
   } else {
+    // const resData = await response.json();
+    // return resData.events;
     const resData = await response.json();
     return resData.events;
   }
 };
 
 export const loader = () => {
-  defer({
+  return defer({
     events: loadEvents(),
   });
 };
