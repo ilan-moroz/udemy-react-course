e78c78b398f859c0c8dc84bf82495bcb2fc166f9
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-28 13:55:34 +0300

    lazy

--- deploying/src/App.js
+++ deploying/src/App.js
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
 import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
+import React from 'react';
+
+const BlogPage = React.lazy(() => import('./pages/Blog'));
 
 const router = createBrowserRouter([
   {
@@ -17,7 +19,16 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
+          {
+            index: true,
+            element: (
+              <React.Suspense fallback={<p>loading...</p>}>
+                <BlogPage />
+              </React.Suspense>
+            ),
+            loader: () =>
+              import('./pages/Blog').then(module => module.loader()),
+          },
           { path: ':id', element: <PostPage />, loader: postLoader },
         ],
       },
