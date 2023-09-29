94366f0f13f08b2bc10544d4d2279f603edd1d54
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-29 17:39:04 +0300

    isPending, isError, error

--- tanstack-query/src/components/Events/NewEvent.jsx
+++ tanstack-query/src/components/Events/NewEvent.jsx
@@ -3,11 +3,12 @@ import { useMutation } from '@tanstack/react-query';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { createNewEvent } from '../../utils/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  const { mutate } = useMutation({
+  const { mutate, isPending, isError, error } = useMutation({
     mutationFn: createNewEvent,
   });
 
@@ -18,15 +19,24 @@ export default function NewEvent() {
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && 'Submitting...'}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to create event!"
+          message={error.info?.message || 'Check Ur Inputs'}
+        />
+      )}
     </Modal>
   );
 }
