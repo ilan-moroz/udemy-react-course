32c1fae4f7bb8cf1d932709a6b72cee7875277d5
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-30 17:41:11 +0300

    lodaer action

--- tanstack-query/src/components/Events/EditEvent.jsx
+++ tanstack-query/src/components/Events/EditEvent.jsx
@@ -1,10 +1,16 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useParams,
+  useSubmit,
+  useNavigation,
+} from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { fetchEvent, updateEvent, queryClient } from '../../utils/http.js';
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
@@ -12,33 +18,37 @@ export default function EditEvent() {
   const params = useParams();
   const id = params.id;
 
-  const { data, isPending, isError, error } = useQuery({
+  const submit = useSubmit();
+  const { state } = useNavigation();
+
+  const { data, isError, error } = useQuery({
     queryKey: ['events', id],
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async data => {
-      const newEvent = data.event;
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async data => {
+  //     const newEvent = data.event;
 
-      await queryClient.cancelQueries({ queryKey: ['events', id] });
-      const preEvent = queryClient.getQueryData(['events', id]);
-      queryClient.setQueryData(['events', id], newEvent);
+  //     await queryClient.cancelQueries({ queryKey: ['events', id] });
+  //     const preEvent = queryClient.getQueryData(['events', id]);
+  //     queryClient.setQueryData(['events', id], newEvent);
 
-      return { preEvent };
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(['events', id], context.preEvent);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(['events', id]);
-    },
-  });
+  //     return { preEvent };
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(['events', id], context.preEvent);
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(['events', id]);
+  //   },
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id, event: formData });
-    navigate('../');
+    // mutate({ id, event: formData });
+    // navigate('../');
+    submit(formData, { method: 'PUT' });
   }
 
   function handleClose() {
@@ -46,13 +56,6 @@ export default function EditEvent() {
   }
 
   let content;
-
-  if (isPending)
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
 
   if (isError)
     content = (
@@ -72,14 +75,35 @@ export default function EditEvent() {
   if (data)
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === 'submitting' ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
 
   return <Modal onClose={handleClose}>{content}</Modal>;
 }
+
+export const loader = ({ params }) => {
+  return queryClient.fetchQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
+};
+
+export const action = async ({ request, params }) => {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(['events']);
+  return redirect('../');
+};
