144fe7b4825b1d2aafd35eb3563f96e4f275dbf8
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-09-30 14:29:15 +0300

    display and delete

--- tanstack-query/src/components/Events/EventDetails.jsx
+++ tanstack-query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,31 @@
-import { Link, Outlet } from 'react-router-dom';
-
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import Header from '../Header.jsx';
+import { deleteEvent, fetchEvent } from '../../utils/http.js';
+import { queryClient } from '../../utils/http.js';
 
 export default function EventDetails() {
+  const navigate = useNavigate();
+  const params = useParams();
+  const id = params.id;
+
+  const { data, isPending } = useQuery({
+    queryKey: ['event'],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    },
+  });
+
+  const handleDelete = () => {
+    mutate({ id });
+  };
+
   return (
     <>
       <Outlet />
@@ -11,25 +34,30 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <p>Loading...</p>}
+      {!isPending && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button onClick={handleDelete}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt="" />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.date} @ {data.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
--- tanstack-query/src/utils/http.js
+++ tanstack-query/src/utils/http.js
@@ -6,7 +6,9 @@ export async function fetchEvents({ signal, searchTerm }) {
   console.log(searchTerm);
   let url = 'http://localhost:3000/events';
 
-  if (searchTerm) url += '?search=' + searchTerm;
+  if (searchTerm) {
+    url += '?search=' + searchTerm;
+  }
 
   const response = await fetch(url, { signal: signal });
 
@@ -58,4 +60,36 @@ export async function fetchSelectableImages({ signal }) {
   const { images } = await response.json();
 
   return images;
+}
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
 }
