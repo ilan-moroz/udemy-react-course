44b69981653bfe88309ee0b21e24a01ca9745ae8
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-01 15:15:53 +0300

    MeetupDetail

---
+++ nextJS/nextjs-second-step/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      img="https://st.depositphotos.com/1146092/2514/i/450/depositphotos_25143517-stock-photo-cool-dog.jpg"
+      title="Dog GANG Meetup"
+      address="Dog city Kitten street 6"
+      description="GANG GANG"
+    />
+  );
+};
+
+export default MeetupDetails;
