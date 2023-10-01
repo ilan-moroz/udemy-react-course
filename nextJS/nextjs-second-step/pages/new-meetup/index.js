25271a8696e4876dfe5187e92b7710eb84eaa568
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-01 19:11:33 +0300

    fetch

--- nextJS/nextjs-second-step/pages/new-meetup/index.js
+++ nextJS/nextjs-second-step/pages/new-meetup/index.js
@@ -1,8 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
-  const addMeetup = data => {
-    console.log('🚀 ~ file: index.js:5 ~ NewMeetupPage ~ data:', data);
+  const router = useRouter();
+
+  const addMeetup = async data => {
+    const res = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const responseData = await res.json();
+    console.log(
+      '🚀 ~ file: index.js:9 ~ addMeetup ~ responseData:',
+      responseData
+    );
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetup} />;
