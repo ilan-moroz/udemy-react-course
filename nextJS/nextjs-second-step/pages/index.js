d320cb202589e5ad2af9a2b6173d57646cf84aac
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-01 14:22:05 +0300

    pages

---
+++ nextJS/nextjs-second-step/pages/index.js
@@ -0,0 +1,27 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'Dog GANG Meetup',
+    image:
+      'https://st.depositphotos.com/1146092/2514/i/450/depositphotos_25143517-stock-photo-cool-dog.jpg',
+    address: 'Dog city Kitten street 69',
+    description: 'GANG GANG',
+  },
+
+  {
+    id: 'm2',
+    title: 'cat GANG Meetup',
+    image:
+      'https://steamuserimages-a.akamaihd.net/ugc/1644340994747007967/853B20CD7694F5CF40E83AAC670572A3FE1E3D35/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+    address: 'cat city puppy street 69',
+    description: 'MEOW MEOW',
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
