250311e719c58f524f619c36316639f13f250b1e
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-01 19:44:20 +0300

    getStaticProps api

--- nextJS/nextjs-second-step/pages/[meetupId]/index.js
+++ nextJS/nextjs-second-step/pages/[meetupId]/index.js
@@ -1,34 +1,57 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { MongoClient, ObjectId } from 'mongodb';
 
 const MeetupDetails = props => {
   return (
     <MeetupDetail
-      img="https://st.depositphotos.com/1146092/2514/i/450/depositphotos_25143517-stock-photo-cool-dog.jpg"
-      title="Dog GANG Meetup"
-      address="Dog city Kitten street 6"
-      description="GANG GANG"
+      img={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
-export const getStaticPaths = () => {
+export const getStaticPaths = async () => {
+  const client = await MongoClient.connect(
+    'mongodb+srv://ilan-udemy:8XbJo8T5X07XExZA@udemy-react.2rqqdtp.mongodb.net/'
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [{ params: { meetupId: 'm1' } }, { params: { meetupId: 'm2' } }],
+    paths: meetups.map(meetup => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 };
 
 export const getStaticProps = async context => {
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(
+    'mongodb+srv://ilan-udemy:8XbJo8T5X07XExZA@udemy-react.2rqqdtp.mongodb.net/'
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  const meetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image: '',
-        title: '',
-        address: '',
-        description: '',
+        id: meetup._id.toString(),
+        title: meetup.title,
+        address: meetup.address,
+        description: meetup.description,
+        image: meetup.image,
       },
     },
     revalidate: 10,
