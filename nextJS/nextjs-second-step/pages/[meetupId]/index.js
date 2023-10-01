631d8d6278574ed07f2443e17a4d8d8acd21fbc1
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-01 20:19:29 +0300

    blocking

--- nextJS/nextjs-second-step/pages/[meetupId]/index.js
+++ nextJS/nextjs-second-step/pages/[meetupId]/index.js
@@ -30,7 +30,7 @@ export const getStaticPaths = async () => {
   client.close();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map(meetup => ({
       params: { meetupId: meetup._id.toString() },
     })),
