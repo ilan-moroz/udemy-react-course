77718edb9cb0fa17efb6a7e52cc9cac0f2f26a6b
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-01 13:59:32 +0300

    Link

--- nextJS/nextjs-start/pages/news/index.js
+++ nextJS/nextjs-start/pages/news/index.js
@@ -1,5 +1,17 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-great">NextJS is Great🔥</Link>
+        </li>
+        <li>bla bla</li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
