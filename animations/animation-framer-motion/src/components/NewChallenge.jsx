b2b7166dd2e292e2eff0be5d229e02d56e66554a
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-15 16:02:14 +0300

    motion

--- animations/animation-framer-motion/src/components/NewChallenge.jsx
+++ animations/animation-framer-motion/src/components/NewChallenge.jsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useState } from 'react';
-
+import { motion } from 'framer-motion';
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import Modal from './Modal.jsx';
 import images from '../assets/images.js';
@@ -57,14 +57,20 @@ export default function NewChallenge({ onDone }) {
         </p>
 
         <ul id="new-challenge-images">
-          {images.map((image) => (
-            <li
+          {images.map(image => (
+            <motion.li
+              variants={{
+                hidden: { opacity: 0, scale: 0.5 },
+                visible: { opacity: 1, scale: 1 },
+              }}
+              exit={{ opacity: 1, scale: 1 }}
+              transition={{ type: 'spring' }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
               className={selectedImage === image ? 'selected' : undefined}
             >
-              <img {...image} />
-            </li>
+              <motion.img {...image} whileHover={{ scale: 1.2 }} />
+            </motion.li>
           ))}
         </ul>
 
