199c9b601536de78d5cb3ba0639bd1e98fa0268a
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-15 15:50:34 +0300

    transition spring

--- animations/animation-framer-motion/src/components/Header.jsx
+++ animations/animation-framer-motion/src/components/Header.jsx
@@ -22,6 +22,7 @@ export default function Header() {
         <h1>Your Challenges</h1>
         <motion.button
           whileHover={{ scale: 1.1 }}
+          transition={{ type: 'spring', stiffness: 500 }}
           onClick={handleStartAddNewChallenge}
           className="button"
         >
