0a76c124d50e0e89f878f52c516dc294d9a214c8
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-23 13:52:55 +0300

    more tests

--- testing/src/components/Greeting.js
+++ testing/src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(prev => !prev);
+  };
+
   return (
     <>
       <h2>hello world!</h2>
-      <p>Its nice to see u</p>
+      {!changedText && <p>Its nice to see u</p>}
+      {changedText && <p>Changed😉</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </>
   );
 };
--- testing/src/components/Greeting.test.js
+++ testing/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
+import userEvent from '@testing-library/user-event';
 
 describe('Greeting comp', () => {
   test('renders hello world text', () => {
@@ -12,5 +13,24 @@ describe('Greeting comp', () => {
     // assert
     const helloWorldElement = screen.getByText(/hello world/i);
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders nice to see if button not clicked', () => {
+    render(<Greeting />);
+    const pText = screen.getByText('Its nice to see u');
+    expect(pText).toBeInTheDocument();
+  });
+
+  test('renders changed if button was clicked', () => {
+    // arrange
+    render(<Greeting />);
+
+    //   act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // assert
+    const pText = screen.getByText(/Changed/i);
+    expect(pText).toBeInTheDocument();
   });
 });
