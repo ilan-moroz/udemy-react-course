4c0e240c0936d9ec94026e47afe99fbf0a5ab1ee
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-10-23 13:42:08 +0300

    describe

--- testing/src/components/Greeting.test.js
+++ testing/src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders hello world text', () => {
-  // arrange
-  render(<Greeting />);
+describe('Greeting comp', () => {
+  test('renders hello world text', () => {
+    // arrange
+    render(<Greeting />);
 
-  //   act
-  // ... nothing
+    //   act
+    // ... nothing
 
-  // assert
-  const helloWorldElement = screen.getByText(/hello world/i);
-  expect(helloWorldElement).toBeInTheDocument();
+    // assert
+    const helloWorldElement = screen.getByText(/hello world/i);
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
