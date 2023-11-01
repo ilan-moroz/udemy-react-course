85285a50f2c6c1e59c0e38b16134ef6bba782419
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-01 11:53:25 +0200

    edit

---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const Player = ({ name, symbol }) => {
+  const [isEdit, setIsEdit] = useState(false);
+
+  const editHandler = () => {
+    setIsEdit(prev => !prev);
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {!isEdit ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text"></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={editHandler}> {isEdit ? 'Save' : 'Edit'}</button>
+    </li>
+  );
+};
+
+export default Player;
