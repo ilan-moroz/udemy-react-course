d7ad103d123eeffa02373dd87835a190c2a8b3c3
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-01 12:07:02 +0200

    changeHandler

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -2,18 +2,28 @@ import { useState } from 'react';
 
 const Player = ({ name, symbol }) => {
   const [isEdit, setIsEdit] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
 
   const editHandler = () => {
     setIsEdit(prev => !prev);
+  };
+
+  const changeHandler = e => {
+    setPlayerName(e.target.value);
   };
 
   return (
     <li>
       <span className="player">
         {!isEdit ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         ) : (
-          <input type="text" required></input>
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={changeHandler}
+          ></input>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
