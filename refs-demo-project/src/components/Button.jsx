95d53e95d8db5e14997102e561e3c3388223dd9c
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-04 12:43:14 +0200

    tailwind

---
+++ refs-demo-project/src/components/Button.jsx
@@ -0,0 +1,8 @@
+const Button = ({ children }) => {
+  return (
+    <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-slate-100 ">
+      {children}
+    </button>
+  );
+};
+export default Button;
