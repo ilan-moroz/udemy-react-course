2fe8030597db0c940fed2644e75c73965219810a
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-05 13:20:54 +0200

    delete

--- refs-demo-project/src/App.jsx
+++ refs-demo-project/src/App.jsx
@@ -51,10 +51,24 @@ function App() {
     });
   };
 
+  const handleDelete = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          project => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  };
+
   const selectedProject = projectsState.projects.find(
     project => project.id === projectsState.selectedProjectId
   );
-  let content = <SelectedProject project={selectedProject} />;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDelete} />
+  );
 
   if (projectsState.selectedProjectId === null)
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancel} />;
