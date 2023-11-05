49a189a5135dd260383bce35a084f77b48771ccf
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-05 16:28:47 +0200

    setInterval

--- side-effects-new/src/components/DeleteConfirmation.jsx
+++ side-effects-new/src/components/DeleteConfirmation.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { useEffect } from 'react';
 
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime(prev => prev - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       clearTimeout(timer);
@@ -23,6 +38,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
