0a7c976f4a12040aa43b85b1c49355f4c2449e81
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-05 17:26:19 +0200

    header & quiz

---
+++ quiz-app/src/components/Quiz.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+const Quiz = () => {
+  const [curQuestion, setCurQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState(0);
+
+  return <p>Question</p>;
+};
+
+export default Quiz;
