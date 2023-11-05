c12555e8b17fe05255ea84c44e132f91401019fb
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-05 16:09:08 +0200

    clean up

--- side-effects-new/src/components/Modal.jsx
+++ side-effects-new/src/components/Modal.jsx
@@ -12,7 +12,7 @@ const Modal = ({ children, open, onClose }) => {
 
   return createPortal(
     <dialog className="modal" ref={dialog} onClose={onClose}>
-      {children}
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
