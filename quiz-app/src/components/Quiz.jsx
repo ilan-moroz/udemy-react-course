adda7acaf85b05d0ab749feaa77c471dfd72e869
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-05 17:50:02 +0200

    quizIsOver?

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions.js';
+import completeImg from '../assets/quiz-complete.png';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const curQuestionIndex = userAnswers.length;
-  const shuffleAnswers = [...QUESTIONS[curQuestionIndex].answers];
-  shuffleAnswers.sort(() => Math.random() - 0.5);
 
   const handleAnswerSelect = answer => {
     setUserAnswers(prevAnswers => [...prevAnswers, answer]);
   };
+
+  const quizIsOver = curQuestionIndex === QUESTIONS.length;
+
+  if (quizIsOver)
+    return (
+      <div id="summary">
+        <img src={completeImg} alt="trophy" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+
+  const shuffleAnswers = [...QUESTIONS[curQuestionIndex].answers];
+  shuffleAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
