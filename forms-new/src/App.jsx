4249d5a3ea80d52555ed6cb383f52c0d99f89dc7
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-07 16:09:34 +0200

    FormData

---
+++ forms-new/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header.jsx';
+import Login from './components/Login.jsx';
+import SignUp from './components/Signup.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        {/* <Login /> */}
+        <SignUp />
+      </main>
+    </>
+  );
+}
+
+export default App;
