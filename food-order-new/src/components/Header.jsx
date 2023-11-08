b16a9dd413e413287cb00c0fa68e08a767623820
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-08 17:27:12 +0200

    totalCartItems

--- food-order-new/src/components/Header.jsx
+++ food-order-new/src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { useContext } from 'react';
 import logo from '../assets/logo.jpg';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
 
 const Header = () => {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartItems = cartCtx.items.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
   return (
     <header id='main-header'>
       <div id='title'>
@@ -9,7 +18,7 @@ const Header = () => {
         <h1>MorozFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
