0bc4499444ed277a7a863dd43ccbc0323e2e0757
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-08 15:44:04 +0200

    button comp

---
+++ food-order-new/src/components/UI/Button.jsx
@@ -0,0 +1,12 @@
+const Button = ({ children, textOnly, className, ...props }) => {
+  let cssClass = textOnly ? 'text-button' : 'button';
+  cssClass += ' ' + className;
+
+  return (
+    <button className={cssClass} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
