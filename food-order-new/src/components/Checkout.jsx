e63abeb4c904c5782b84be728494247439839c69
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-11-09 16:06:11 +0200

    fetch

--- food-order-new/src/components/Checkout.jsx
+++ food-order-new/src/components/Checkout.jsx
@@ -19,16 +19,36 @@ const Checkout = () => {
     userCtx.hideCheckout();
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const fData = new FormData(e.target);
+    const data = Object.fromEntries(fData.entries());
+
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: data,
+        },
+      }),
+    });
+  };
+
   return (
     <Modal open={userCtx.progress === 'checkout'} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input type='text' label='Full Name' id='fullName' />
+        <Input type='text' label='Full Name' id='name' />
         <Input type='email' label='Email Address' id='email' />
         <Input type='text' label='Street' id='street' />
         <div className='control-row'>
-          <Input type='text' label='Postal Code' id='postalCode' />
+          <Input type='text' label='Postal Code' id='postal-code' />
           <Input type='text' label='City' id='city' />
         </div>
         <p className='modal-actions'>
